b22778ac4d76f13d945e2a8936ee56352d1ea59a
Samir Rajyaguru <samirphp2016@gmail.com>
2021-08-12 09:38:25 +0530

    Initial Commit Test

---
+++ 404.php
@@ -0,0 +1,29 @@
+<?php 
+	include('backoffice/autoload.php');
+    include('backoffice/include/function.php');
+	
+	include 'v2_header.php';
+?>
+<style>
+.c-content{
+	margin: 5% 15px;
+}
+.c-content p{	
+	font-size: 1.2rem;
+}
+.content__box{
+	min-height:80vh;
+}
+.content__box--title{
+	float: unset;
+}
+</style>
+<div class="content__box">
+	<div class="content__box--title">
+		<p>Page Not found </p>
+	</div>
+	<div class="c-content">
+	404 Page not found 
+	</div>
+</div>	
+<?php include 'v2_footer.php';?>
